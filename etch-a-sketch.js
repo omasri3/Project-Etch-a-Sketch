931afd1faf6be84867463c2a9e749a78fdcc6056
Omar Masri <16193809+omasri3@users.noreply.github.com>
2025-06-13 16:32:52 -0400

    adds hovering changes color to the etch and sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,15 @@
+//Grab the grid container and add in the 8x8 row
 const grid = document.querySelector("#gridContainer");
 
 for (let i = 0; i < 8; i++) {
     let singleGridRow = document.createElement("div");
     for (let i = 0; i < 8; i++){
         let singleGridUnit = document.createElement("div")
-        singleGridUnit.className = "singleGridUnit"
+        singleGridUnit.classList.add("singleGridUnit")
+        singleGridUnit.addEventListener("mouseenter", ()=>{
+            singleGridUnit.classList.add("hovered")
+            console.log("added hovered class")
+        })
         singleGridRow.appendChild(singleGridUnit);
     }
     singleGridRow.className = "singleGridRow";
